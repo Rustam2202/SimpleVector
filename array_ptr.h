4e2ecf094cfea72eb104be8c989911843f0215e0
Рустам Нурмиев <Rustam2202@yandex.ru>
2022-01-13 12:24:19 +0300

    v1

--- array_ptr.h
+++ array_ptr.h
@@ -6,7 +6,6 @@ public:
 
     // Создаёт в куче массив из size элементов типа Type. Если size == 0, поле raw_ptr_ должно быть равно nullptr
     explicit ArrayPtr(size_t size) {
-        // Реализуйте конструктор самостоятельно
         if (!size == 0) {
             Type* arr = new Type[size];
             raw_ptr_ = arr;
@@ -15,7 +14,6 @@ public:
 
     // Конструктор из сырого указателя, хранящего адрес массива в куче либо nullptr
     explicit ArrayPtr(Type* raw_ptr) noexcept {
-        // Реализуйте конструктор самостоятельно
         raw_ptr_ = raw_ptr;
     }
 
@@ -23,44 +21,36 @@ public:
     ArrayPtr(const ArrayPtr&) = delete;
 
     ~ArrayPtr() {
-        // Напишите деструктор самостоятельно
         delete[]raw_ptr_;
     }
 
     // Запрещаем присваивание
     ArrayPtr& operator=(const ArrayPtr&) = delete;
 
-    // Прекращает владением массивом в памяти, возвращает значение адреса массива
-    // После вызова метода указатель на массив должен обнулиться
+    // Прекращает владением массивом в памяти, возвращает значение адреса массива. После вызова метода указатель на массив должен обнулиться
     [[nodiscard]] Type* Release() noexcept {
-        // Заглушка. Реализуйте метод самостоятельно
         Type* temp = raw_ptr_;
-        // delete[]raw_ptr_;
         raw_ptr_ = nullptr;
         return temp;
     }
 
     // Возвращает ссылку на элемент массива с индексом index
     Type& operator[](size_t index) noexcept {
-        // Реализуйте операцию самостоятельно
         return raw_ptr_[index];
     }
 
     // Возвращает константную ссылку на элемент массива с индексом index
     const Type& operator[](size_t index) const noexcept {
-        // Реализуйте операцию самостоятельно
         return raw_ptr_[index];
     }
 
     // Возвращает true, если указатель ненулевой, и false в противном случае
     explicit operator bool() const {
-        // Заглушка. Реализуйте операцию самостоятельно
         return raw_ptr_ != nullptr;
     }
 
     // Возвращает значение сырого указателя, хранящего адрес начала массива
     Type* Get() const noexcept {
-        // Заглушка. Реализуйте метод самостоятельно
         return raw_ptr_;
     }
 
